f7bc9cebe20b69ab5c999d1655cc312a1ea606b6
rafa133169 <127886555+rafa133169@users.noreply.github.com>
2023-07-24 06:37:38 -0600

    Add files via upload
    login y registro

---
+++ vite-project-sm32/src/Pages/login.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import "../css/estilos.css";
+import "./loginRegistrojs";
+
+
+
+function LoginRegistro  ()  {
+  return (
+    <div className="contenedor_todo">
+      <div className="caja_trasera">
+        <div className="caja_trasera_login">
+          <h3>¿Ya tienes una cuenta?</h3>
+          <p>Inicia sesión para entrar a la página</p>
+          <button id="btn iniciar-sesion">Iniciar Sesión</button>
+        </div>
+        <div className="caja_trasera_registro">
+          <h3>¿Aún no tienes una cuenta?</h3>
+          <p>Regístrate para que puedas iniciar sesión</p>
+          <button id="btn registrarse">Registrarse</button>
+        </div>
+      </div>
+      {/* formulario de login y registro */}
+      <div className="contenedor_login-registro">
+        <form action="" className="formulario_login">
+          <h2>Iniciar Sesión</h2>
+          <input type="email" placeholder="Correo Electrónico" />
+          <input type="password" placeholder="Contraseña" />
+          <button>Entrar</button>
+        </form>
+        <form action="php/registro_usuario_BE.php" method="POST" className="formulario_registro">
+          <h2>Registrarse</h2>
+          <input type="text" placeholder="Nombre Completo" name="nombre_completo" />
+          <input type="text" placeholder="Usuario" name="usuario" />
+          <input type="text" placeholder="Correo Electrónico" name="correo" />
+          <input type="password" placeholder="Contraseña" name="contrasena" />
+          <button>Registrarse</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginRegistro;
